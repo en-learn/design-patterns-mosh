4669778135c253744a0e026ec3e3c380c04618d3
Elias Norrby <elias.norrby@gmail.com>
2020-02-25 08:00:42 +0100

    L6: Encapsulation

--- DesignPatterns/src/com/eliasnorrby/Main.java
+++ DesignPatterns/src/com/eliasnorrby/Main.java
@@ -3,11 +3,10 @@ package com.eliasnorrby;
 public class Main {
 
     public static void main(String[] args) {
-      TaxCalculator calculator = getCalculator();
-      calculator.calculateTax();
+      Account account = new Account();
+      account.setBalance(10);
+      account.withdraw(5);
+      System.out.println(account.getBalance());
     }
 
-    public static TaxCalculator getCalculator() {
-      return new TaxCalculator2019();
-    }
 }
