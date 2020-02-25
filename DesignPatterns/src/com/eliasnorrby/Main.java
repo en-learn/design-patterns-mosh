7971c3489c0de900ae49e26a28e6b6ff11fb59b8
Elias Norrby <elias.norrby@gmail.com>
2020-02-25 10:30:11 +0100

    L7: Abstraction

--- DesignPatterns/src/com/eliasnorrby/Main.java
+++ DesignPatterns/src/com/eliasnorrby/Main.java
@@ -3,10 +3,8 @@ package com.eliasnorrby;
 public class Main {
 
     public static void main(String[] args) {
-      Account account = new Account();
-      account.setBalance(10);
-      account.withdraw(5);
-      System.out.println(account.getBalance());
+      var mailService = new MailService();
+      mailService.sendEmail();
     }
 
 }
