9744135c3a3dbc4893c8d4bb12c545074d977999
Elias Norrby <elias.norrby@gmail.com>
2020-03-02 15:08:13 +0100

    L13: Memento - The Implementation

--- DesignPatterns/src/com/eliasnorrby/Main.java
+++ DesignPatterns/src/com/eliasnorrby/Main.java
@@ -1,15 +1,25 @@
 package com.eliasnorrby;
 
 import com.eliasnorrby.memento.Editor;
+import com.eliasnorrby.memento.History;
 
 public class Main {
 
     public static void main(String[] args) {
       var editor = new Editor();
+      var history = new History();
+
       editor.setContent("a");
+      history.push(editor.createState());
+
       editor.setContent("b");
+      history.push(editor.createState());
+
       editor.setContent("c");
-      editor.undo();
+      editor.restore(history.pop());
+      editor.restore(history.pop());
+
+      System.out.println("editor = " + editor.getContent());
     }
 
 }
