11fc3017a3d77f52fdb886b89b77cd0f8ccbf523
Elias Norrby <elias.norrby@gmail.com>
2020-03-09 09:04:09 +0100

    L22: Iterator - Benefits

--- DesignPatterns/src/com/eliasnorrby/iterator/BrowseHistory.java
+++ DesignPatterns/src/com/eliasnorrby/iterator/BrowseHistory.java
@@ -1,43 +1,37 @@
 package com.eliasnorrby.iterator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BrowseHistory {
-  private List<String> urls = new ArrayList<>();
+  private String[] urls = new String[10];
+  private int count;
 
   public void push(String url) {
-    urls.add(url);
+    urls[count++] = url;
   }
 
   public String pop() {
-    var lastIndex = urls.size() - 1;
-    var lastUrl = urls.get(lastIndex);
-    urls.remove(lastUrl);
-
-    return lastUrl;
+    return urls[--count];
   }
 
   public Iterator<String> createIterator() {
-    return new ListIterator(this);
+    return new ArrayIterator(this);
   }
 
-  public class ListIterator implements Iterator<String> {
+  public class ArrayIterator implements Iterator<String> {
     private BrowseHistory history;
     private int index;
 
-    public ListIterator(BrowseHistory history) {
+    public ArrayIterator(BrowseHistory history){
       this.history = history;
     }
 
     @Override
     public boolean hasNext() {
-      return (index < history.urls.size());
+      return (index < history.count);
     }
 
     @Override
     public String current() {
-      return history.urls.get(index);
+      return history.urls[index];
     }
 
     @Override
